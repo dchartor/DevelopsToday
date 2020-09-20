7b9c4b18ea396bba65269f582c7684ed5f8c7ba7
Dmytro Chartorynskyi <dchartorynskyi@gmail.com>
2020-09-20 14:40:55 +0300

    secure_proxy_ssl_header and allowed hosts update

--- hackerNewsProject/settings.py
+++ hackerNewsProject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['hacker-post-api.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0']
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
@@ -169,6 +169,7 @@ if ENVIRONMENT == 'production':
     SECURE_HSTS_SECONDS = 3600
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Heroku
